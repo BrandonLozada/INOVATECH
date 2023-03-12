d501b5d0b876bd6cbe84342c9c7ef0fd4af4002c
BrandonLozada <brandon_lozada@outlook.com>
2023-03-12 03:01:36 -0600

    Implementar ErrorNotFound.vue

--- quasar/inovatech/src/pages/ErrorNotFound.vue
+++ quasar/inovatech/src/pages/ErrorNotFound.vue
@@ -1,25 +1,31 @@
 <template>
-  <div class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center">
-    <div>
-      <div style="font-size: 30vh">
-        404
-      </div>
+  <q-page style="min-height: 650px">
+    <div class="bg-blue text-white text-center q-pa-md flex flex-center">
+      <div>
+        <div style="font-size: 30vh">
+          404
+        </div>
 
-      <div class="text-h2" style="opacity:.4">
-        Oops. Nothing here...
-      </div>
+        <div class="text-h2" style="opacity:.6">
+          No hemos encontrado la página que buscas.
+        </div>
 
-      <q-btn
-        class="q-mt-xl"
-        color="white"
-        text-color="blue"
-        unelevated
-        to="/"
-        label="Go Home"
-        no-caps
-      />
+        <div class="text-subtitle1">
+          Prueba en inicio con la barra de menú lateral de opciones de Inovatech App.
+        </div>
+
+        <q-btn
+          class="q-mt-xl"
+          color="white"
+          text-color="blue"
+          unelevated
+          to="/"
+          label="Ir a Inicio"
+          no-caps
+        />
+      </div>
     </div>
-  </div>
+  </q-page>
 </template>
 
 <script setup lang="ts">
