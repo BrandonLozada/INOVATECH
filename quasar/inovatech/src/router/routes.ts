97c9ecc5900ec5f2312c2530031fedffcd370233
BrandonLozada <brandon_lozada@outlook.com>
2023-03-18 16:21:38 -0600

    Agregar nuevas rutas

--- quasar/inovatech/src/router/routes.ts
+++ quasar/inovatech/src/router/routes.ts
@@ -4,14 +4,46 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    children: [
+      { path: '', name: 'Home', component: () => import('pages/IndexPage.vue') },
+      { path: '/signup', component: () => import('pages/SignUp.vue') },
+      { path: '/signin', component: () => import('pages/SignIn.vue') },
+      { path: '/assistance-control', component: () => import('pages/FingerprintAttendance.vue') },
+
+      // This will be show information about firebase or another thing
+      { path: 'my-user/:id', component: () => import('pages/UserInformation.vue') },
+
+      // The next path 'my-profile' will show all the components below (profile, data, addresses, cards, payment methods, etc.)
+      { path: 'my-profile', component: () => import('pages/UserProfile.vue') },
+      { path: 'my-data', component: () => import('pages/UserData.vue') },
+      { path: 'my-addresses', component: () => import('pages/UserAddresses.vue') },
+
+      {
+        path: '/forms/',
+        name: 'Forms',
+        component: () => import('pages/PageForms.vue'),
+        children: [
+          {
+            path: '/:catchAll(.*)*',
+            component: () => import('pages/PageForms.vue'),
+          },
+        ],
+      },
+      {
+        path: '/:catchAll(.*)*',
+        component: () => import('pages/IndexPage.vue'),
+      },
+    ],
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue'),
+    path: '/error404',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      {path: '', component: () => import('pages/ErrorNotFound.vue')},
+    ],
   },
 ];
 
