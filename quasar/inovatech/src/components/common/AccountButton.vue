acdc3fb4dba8c522af3de791979901fa98859d42
BrandonLozada <brandon_lozada@outlook.com>
2023-03-18 16:47:02 -0600

    Botones de la barra de navegación

---
+++ quasar/inovatech/src/components/common/AccountButton.vue
@@ -0,0 +1,34 @@
+<template>
+  <q-btn
+    flat
+    dense
+    round
+    icon="person"
+    aria-label="Cuenta"
+  >
+  <q-tooltip :delay="3000">Mi cuenta</q-tooltip>
+
+    <q-menu anchor="top middle" self="bottom middle" class="z-top"
+            transition-show="jump-down"
+            transition-hide="jump-up"
+            style="width: 350px; min-width: 350px; height: auto;">
+      <q-card class="my-card">
+
+      </q-card>
+    </q-menu>
+  </q-btn>
+
+
+</template>
+
+<script setup lang="ts">
+import { ref} from 'vue'
+import {useQuasar} from 'quasar'
+import {useRouter} from 'vue-router'
+
+const $q = useQuasar()
+const router = useRouter()
+const showPassword = ref(false);
+</script>
+
+<style scoped></style>
---
+++ quasar/inovatech/src/components/common/NotificationButton.vue
@@ -0,0 +1,33 @@
+<template>
+  <q-btn
+    flat
+    dense
+    round
+    icon="notifications"
+    aria-label="Notificaciones"
+  >
+    <q-badge color="red" floating>2</q-badge>
+  <q-tooltip :delay="3000">Mis notificaciones</q-tooltip>
+
+    <q-menu anchor="top middle" self="bottom middle" class="z-top"
+            transition-show="jump-down"
+            transition-hide="jump-up"
+            style="width: 350px; min-width: 350px; height: auto;">
+      <q-card class="my-card">
+
+      </q-card>
+    </q-menu>
+  </q-btn>
+
+
+</template>
+
+<script setup lang="ts">
+import {useQuasar} from 'quasar'
+import {useRouter} from 'vue-router'
+
+const $q = useQuasar()
+const router = useRouter()
+</script>
+
+<style scoped></style>
