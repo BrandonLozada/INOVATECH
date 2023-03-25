201cee4b363005a5d02643131317ce16a3a2e30b
BrandonLozada <brandon_lozada@outlook.com>
2023-03-25 17:55:44 -0600

    Reestructurar las rutas y los layouts

---
+++ quasar/inovatech/src/layouts/SecondaryLayout.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-layout view="lhr LpR lfr">
+
+<!-- TODO: Aquí se puede utilizar un state de pinia para detectar la route,
+           igualarla, hacer el .match para hacer un ruteo y un boleano para pintar este componente. -->
+    <PrincipalNavigation v-if="this.$route.path !== '/signin' && this.$route.path !== '/signin/'"/>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <!-- TODO: Aquí se puede utilizar un state de pinia para detectar la route,
+           igualarla, hacer el .match para hacer un ruteo y un boleano para pintar este componente. -->
+<!--    <PrincipalFooter></PrincipalFooter>-->
+
+  </q-layout>
+</template>
+
+<script setup lang="ts">
+import PrincipalFooter from 'components/common/PrincipalFooter.vue';
+import PrincipalNavigation from 'components/common/PrincipalNavigation.vue';
+</script>
--- quasar/inovatech/src/router/routes.ts
+++ quasar/inovatech/src/router/routes.ts
@@ -8,7 +8,7 @@ const routes: RouteRecordRaw[] = [
       { path: '', name: 'Home', component: () => import('pages/IndexPage.vue') },
       // TODO: La página de la acción de crear cuenta es ejecutada por un administrador y pasaría a ser un formulario, procedimental.
       { path: '/signup', component: () => import('pages/SignUp.vue') },
-      { path: '/signin', component: () => import('pages/SignIn.vue') },
+      // TODO: Crear la página (y remplazar el .vue) dónde se visualice el control de asistencia de los usuarios empleados.
       { path: '/assistance-control', component: () => import('pages/FingerprintAttendance.vue') },
 
       // This will be show information about firebase or another thing
@@ -19,24 +19,49 @@ const routes: RouteRecordRaw[] = [
       { path: 'my-data', component: () => import('pages/UserData.vue') },
       { path: 'my-addresses', component: () => import('pages/UserAddresses.vue') },
 
-      {
-        path: '/forms/',
-        name: 'Forms',
-        component: () => import('pages/PageForms.vue'),
-        children: [
-          {
-            path: '/:catchAll(.*)*',
-            component: () => import('pages/PageForms.vue'),
-          },
-        ],
-      },
-      {
-        path: '/:catchAll(.*)*',
-        component: () => import('pages/IndexPage.vue'),
-      },
+      // {
+      //   path: '/forms/',
+      //   name: 'Forms',
+      //   component: () => import('pages/PageForms.vue'),
+      //   children: [
+      //     {
+      //       path: '/:catchAll(.*)*',
+      //       component: () => import('pages/PageForms.vue'),
+      //     },
+      //   ],
+      // },
+      // { path: '/:catchAll(.*)*', component: () => import('pages/IndexPage.vue') },
     ],
   },
 
+  // TODO: Intentar crear la ruta dinamica '/forms/:formulario' para la siguiente ruta.
+  // Formularios user, profile employee, address
+  {
+    path: '/forms',
+    name: 'Forms',
+    component: () => import('layouts/SecondaryLayout.vue'),
+    children: [
+      { path: '', name: 'Formulario', component: () => import('pages/PageForms.vue') },
+      { path: '/', component: () => import('pages/PageForms.vue') },
+      // TODO: Aquí poner el componente de gestión de usuarios o listado de empleados dependiendo del rol de usuario.
+      { path: '/:catchAll(.*)*', component: () => import('pages/PageForms.vue') },
+    ],
+  },
+  {
+    path: '/signin',
+    component: () => import('layouts/SecondaryLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/SignIn.vue') },
+      { path: '/:catchAll(.*)*', component: () => import('pages/SignIn.vue') },
+    ],
+  },
+  {
+    path: '/fingerprint-attendance',
+    component: () => import('layouts/SecondaryLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/FingerprintAttendance.vue') },
+    ],
+  },
   // Always leave this as last one,
   // but you can also remove it
   {
