eaa87e03f0d433dc47f2d5a296f265ed49500143
BrandonLozada <brandon_lozada@outlook.com>
2023-04-24 01:39:02 -0600

    Agregar plugins de quasar y configuraciones.

--- quasar/inovatech/quasar.config.js
+++ quasar/inovatech/quasar.config.js
@@ -98,7 +98,12 @@ module.exports = configure(function (/* ctx */) {
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#framework
     framework: {
-      config: {},
+      config: {
+        loadingBar: {
+          size: '5px',
+          position: 'top'
+        }
+      },
 
       // iconSet: 'material-icons', // Quasar icon set
       // lang: 'en-US', // Quasar language pack
@@ -112,7 +117,12 @@ module.exports = configure(function (/* ctx */) {
       // directives: [],
 
       // Quasar plugins
-      plugins: []
+      plugins: [
+        'LoadingBar',
+        'Dialog',
+        'Loading',
+        'Notify',
+      ],
     },
 
     animations: 'all', // --- includes all animations
