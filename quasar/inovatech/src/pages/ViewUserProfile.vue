78039953eec6e4b2f2ac13af56cc301b1b606b37
BrandonLozada <brandon_lozada@outlook.com>
2023-05-16 00:42:29 -0600

    Cambios.

--- quasar/inovatech/src/pages/ViewUserProfile.vue
+++ quasar/inovatech/src/pages/ViewUserProfile.vue
@@ -11,80 +11,81 @@
       </div>
 
       <q-card class="my-card col-12 q-mb-lg">
-            <q-list separator>
+        <q-list separator>
+          <q-item-label
+            header
+            class="text-black text-weight-regular"
+            style="font-weight: 500; font-size: 1rem; margin: 2px;">Información personal
+          </q-item-label>
 
-        <q-item-label header class="text-black text-weight-regular"
-                      style="font-weight: 500; font-size: 1rem; margin: 2px;">Información personal
-        </q-item-label>
+          <q-btn
+            @click="router.push({'path':`/edit-user/${route.params.id}`})"
+            label="Editar"
+            flat
+            color="grey-7"
+            class="absolute-top-right bg-grey-2"
+            style="top: 0; right: 15px; transform: translateY(15px);">
+          </q-btn>
 
-        <q-btn
-          @click="router.push({'path':`/edit-user/${route.params.id}`})"
-          label="Editar"
-          flat
-          color="grey-7"
-          class="absolute-top-right bg-grey-2"
-          style="top: 0; right: 15px; transform: translateY(15px);"
-        ></q-btn>
+          <q-item v-if="myUserData.nombre_completo != null">
+            <q-item-section class="col-3">
+              <q-item-label caption class="q-mt-sm" style="font-weight: 500;">
+                Nombre completo
+              </q-item-label>
+            </q-item-section>
 
-        <q-item v-if="myUserData.nombre_completo != null">
-          <q-item-section class="col-3">
-            <q-item-label caption class="q-mt-sm" style="font-weight: 500;">
-              Nombre completo
-            </q-item-label>
-          </q-item-section>
+            <q-item-section>
+              <q-item-label class="q-mt-sm q-ml-lg text-grey-10"> {{ myUserData.nombre_completo }}</q-item-label>
+            </q-item-section>
+          </q-item>
 
-          <q-item-section>
-            <q-item-label class="q-mt-sm q-ml-lg text-grey-10"> {{ myUserData.nombre_completo }}</q-item-label>
-          </q-item-section>
-        </q-item>
+          <q-item v-if="myUserData.fecha_nacimiento != null">
+            <q-item-section class="col-3">
+              <q-item-label caption class="q-mt-sm" style="font-weight: 500;">
+                Fecha de nacimiento
+              </q-item-label>
+            </q-item-section>
 
-        <q-item v-if="myUserData.fecha_nacimiento != null">
-          <q-item-section class="col-3">
-            <q-item-label caption class="q-mt-sm" style="font-weight: 500;">
-              Fecha de nacimiento
-            </q-item-label>
-          </q-item-section>
+            <q-item-section>
+              <q-item-label class="q-mt-sm q-ml-lg text-grey-10">{{ myUserData.fecha_nacimiento }}</q-item-label>
+            </q-item-section>
+          </q-item>
 
-          <q-item-section>
-            <q-item-label class="q-mt-sm q-ml-lg text-grey-10">{{ myUserData.fecha_nacimiento }}</q-item-label>
-          </q-item-section>
-        </q-item>
+          <q-item v-if="myUserData.sexo != null">
+            <q-item-section class="col-3">
+              <q-item-label caption class="q-mt-sm" style="font-weight: 500;">
+                Sexo
+              </q-item-label>
+            </q-item-section>
 
-        <q-item v-if="myUserData.sexo != null">
-          <q-item-section class="col-3">
-            <q-item-label caption class="q-mt-sm" style="font-weight: 500;">
-              Sexo
-            </q-item-label>
-          </q-item-section>
+            <q-item-section>
+              <q-item-label class="q-mt-sm q-ml-lg text-grey-10">{{ myUserData.sexo }}</q-item-label>
+            </q-item-section>
+          </q-item>
 
-          <q-item-section>
-            <q-item-label class="q-mt-sm q-ml-lg text-grey-10">{{ myUserData.sexo }}</q-item-label>
-          </q-item-section>
-        </q-item>
+          <q-item v-if="myUserData.celular != null">
+            <q-item-section class="col-3">
+              <q-item-label caption class="q-mt-sm" style="font-weight: 500;">
+                Celular
+              </q-item-label>
+            </q-item-section>
 
-        <q-item v-if="myUserData.celular != null">
-          <q-item-section class="col-3">
-            <q-item-label caption class="q-mt-sm" style="font-weight: 500;">
-              Celular
-            </q-item-label>
-          </q-item-section>
+            <q-item-section>
+              <q-item-label class="q-mt-sm q-ml-lg text-grey-10">{{ myUserData.celular }}</q-item-label>
+            </q-item-section>
+          </q-item>
 
-          <q-item-section>
-            <q-item-label class="q-mt-sm q-ml-lg text-grey-10">{{ myUserData.celular }}</q-item-label>
-          </q-item-section>
-        </q-item>
+          <q-item v-if="myUserData.correo != null">
+            <q-item-section class="col-3">
+              <q-item-label caption class="q-mt-sm" style="font-weight: 500;">
+                Correo
+              </q-item-label>
+            </q-item-section>
 
-        <q-item v-if="myUserData.correo != null">
-          <q-item-section class="col-3">
-            <q-item-label caption class="q-mt-sm" style="font-weight: 500;">
-              Correo
-            </q-item-label>
-          </q-item-section>
-
-          <q-item-section>
-            <q-item-label class="q-mt-sm q-ml-lg text-grey-10">{{ myUserData.correo }}</q-item-label>
-          </q-item-section>
-        </q-item>
+            <q-item-section>
+              <q-item-label class="q-mt-sm q-ml-lg text-grey-10">{{ myUserData.correo }}</q-item-label>
+            </q-item-section>
+          </q-item>
             </q-list>
 
         <q-inner-loading :showing="is_loading">
@@ -123,9 +124,13 @@
           ></q-btn>
 
           <template v-if="myEmployeeProfileData != null">
-                      <q-item-label header class="text-black text-weight-regular"
-                        style="font-weight: 500; font-size: 1rem; margin: 2px;">Información de identificación
-          </q-item-label>
+            <q-item-label
+              header
+              class="text-black text-weight-regular"
+              style="font-weight: 500; font-size: 1rem; margin: 2px;">
+              Información de identificación
+            </q-item-label>
+
             <q-item v-if="myEmployeeProfileData.curp != null">
               <q-item-section class="col-3">
                 <q-item-label caption class="q-mt-sm" style="font-weight: 500;">
@@ -306,9 +311,6 @@
 
       </q-card>
     </div>
-
-
-
   </q-page>
 </template>
 
@@ -339,8 +341,6 @@ onBeforeMount(() => {
         }
       }
       ).then(response => {
-        //myUserData.value = response.data.value;
-        // myUserData.value = response.data.value[0];
         console.log('response: ', response)
         console.log('response.data.length: ', response.data.value.length)
 
@@ -349,9 +349,6 @@ onBeforeMount(() => {
         } else {
           myUserData.value = null;
         }
-
-        console.log('myUserData: ', myUserData)
-        console.log('myUserData.value: ', myUserData.value)
       }).then(() => {
         is_loading.value = false
       }).catch(() => {
@@ -369,8 +366,7 @@ onBeforeMount(() => {
       }
       ).then(response => {
         console.log('response: ', response);
-        myEmployeeProfileData.value = response.data.value;
-        console.log('myEmployeeProfileData: ', myEmployeeProfileData);
+        console.log('response.data.length: ', response.data.value.length)
 
         if (response.data.value.length == 1) {
           myEmployeeProfileData.value = response.data.value[0];
